b39077a35f8070fea65d9d992d82bd0d3637b67e
edista <loaner@pentol-im.icehousecorp.local>
2013-10-25 17:13:52 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoodHall::Application.config.secret_key_base = '78ec9df1fa875563a679a27499540cf5ce06d2e408a441ded323651b741ccc38a51fa9cc9417969366d14e842f5678b62c97d2469ee22dd5f459927bde34b409'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FoodHall::Application.config.session_store :cookie_store, key: '_FoodHall_session'
